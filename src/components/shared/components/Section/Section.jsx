c45dec20e7204bc36a456932e2bb361d5e1523bf
Serhii Krasovskyi <chokorik@gmail.com>
2024-09-23 11:17:28 +0300

    start

---
+++ src/components/shared/components/Section/Section.jsx
@@ -0,0 +1,12 @@
+import clsx from "clsx";
+import css from "./Section.module.css";
+
+function Section({ children, className }) {
+  return (
+    <div className={clsx(css.section, { [className]: className })}>
+      {children}
+    </div>
+  );
+}
+
+export default Section;
---
+++ src/pages/CarsPage/CarsPage.jsx
@@ -0,0 +1,7 @@
+import css from "./CarsPage.module.css";
+
+function CarsPage() {
+  return <div></div>;
+}
+
+export default CarsPage;
