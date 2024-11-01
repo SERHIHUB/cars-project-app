0646b203a8c5ec77a34ade5efa2a1f59aa69f26f
Serhii Krasovskyi <chokorik@gmail.com>
2024-11-01 22:38:18 +0200

    in-progres

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import clsx from "clsx";
 import { ModalComponent } from "../shared/components/ModalComponent/ModalComponent";
 import css from "./CarItem.module.css";
@@ -9,13 +9,67 @@ import { DeleteCar } from "../DeleteCar/DeleteCar";
 import { TbListDetails } from "react-icons/tb";
 import { AiOutlineDelete } from "react-icons/ai";
 import { selectCurrentUser } from "../../redux/users/selectors";
+import { updateCar } from "../../redux/cars/operations";
+import { useEffect } from "react";
 
 // const textColor = document.querySelector("#car-title");
 
 export const CarItem = ({ car }) => {
+  const dispatch = useDispatch();
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  // const [isPaid, setIsPaid] = useState(true);
   const location = useLocation();
   const currentUser = useSelector(selectCurrentUser);
+  // ____________________________________
+  const now = new Date();
+  const currentDate = now.getDate();
+  const currentMonth = now.getMonth() + 1;
+
+  const isPaidValue = () => {
+    if (currentMonth <= car.isPaidMonth) return;
+    if (car.isPaid === false) return;
+
+    // const payload = {
+    //   carId: car._id,
+    //   body: {
+    //     isPaid: false,
+    //   },
+    // };
+
+    // console.log(payload);
+    // dispatch(updateCar(payload));
+  };
+
+  // useEffect(() => {
+  //   return isPaidValue();
+  // }, [currentMonth]);
+  // ____________________________________
+
+  const handleButton = () => {
+    const payload = {
+      carId: car._id,
+      body: {
+        isPaid: false,
+      },
+    };
+
+    console.log("payload!!!");
+    dispatch(updateCar(payload));
+  };
+
+  // console.log(car.isPaidMonth);
+  // console.log(currentMonth);
+  // useEffect(() => {}, []);
+  // if (currentDate <= 5) {
+  //   const payload = {
+  //     carId: car._id,
+  //     body: {
+  //       lastPaidDate: car.lastPaidDate,
+  //     },
+  //   };
+  // dispatch(updateCar(payload));
+  //   console.log("isPaid");
+  // }
 
   // textColor.style.color = "red";
 
@@ -46,6 +100,7 @@ export const CarItem = ({ car }) => {
       >{`Model: ${car.carModel}`}</h3>
       <p>{`Number: ${car.carNumber.toUpperCase()}`}</p>
       <p>{`Price: ${car.price}`}</p>
+      <p>{`Paid month: ${car.isPaidMonth}`}</p>
       <p>{`Date of pay: ${car.paymentDate}`}</p>
       {/* <p>{`Contact: ${car.contact !== null ? car.contact : "_ _ _"}`}</p> */}
       <p>{`Contact: ${
@@ -76,6 +131,9 @@ export const CarItem = ({ car }) => {
           </li>
         </ul>
       )}
+      <Button className={css.btn} onClick={handleButton}>
+        BUTTON
+      </Button>
       <ModalComponent closeModal={handleCloseModal} modalIsOpen={modalIsOpen}>
         <DeleteCar car={car} closeModal={handleCloseModal} />
       </ModalComponent>
